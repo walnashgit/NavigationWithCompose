7ade19be31740a7a5426aab260b1460c5e97f043
avinash <avikragarwal@gmail.com>
2026-02-21 14:14:20 +0530

    second commit

--- app/src/main/java/com/speechify/composeuichallenge/ui/BookUI.kt
+++ app/src/main/java/com/speechify/composeuichallenge/ui/BookUI.kt
@@ -1,12 +1,14 @@
 package com.speechify.composeuichallenge.ui
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -43,6 +45,11 @@ fun BookList(modifier: Modifier = Modifier, books: List<Book>) {
                     model = book.imageUrl,
                     contentDescription = ""
                 )
+                Column() {
+                    Text(text = book.name)
+                    Text(text = book.author)
+                    Text(text = "${book.rating} (${book.reviewCount})")
+                }
             }
         }
     }
